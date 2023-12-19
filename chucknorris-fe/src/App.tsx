3fce9f8955538bdbd3ec1c4845e88148f96dc37d
gateremark <mark5gatere@gmail.com>
2023-12-19 12:26:26 +0300

    change ApolloClient uri to localhost

--- chucknorris-fe/src/App.tsx
+++ chucknorris-fe/src/App.tsx
@@ -3,7 +3,7 @@ import Categories from "./components/getAllJokes";
 
 // Set up Apollo Client
 const client = new ApolloClient({
-    uri: "https://chucknorris-jokes-gjcg.onrender.com",
+    uri: "http://localhost:4000",
     cache: new InMemoryCache(),
 });
 
